6b8b01ca2bad534bd7d09cfc3114314cd08cfb6a
Miteto08 <julien.lechevalier@shortways.com>
2025-04-18 09:42:31 +0200

    Task6 - Synchonize Header with Redux store

--- react_redux-part1/task_2/dashboard/src/components/Header/Header.jsx
+++ react_redux-part1/task_2/dashboard/src/components/Header/Header.jsx
@@ -1,22 +1,14 @@
 import './Header.css';
 import logo from '../../assets/holberton-logo.jpg';
-import { useDispatch, useSelector } from 'react-redux';
-import { logout } from '../../features/auth/authSlice';
 
-export default function Header() {
-    const dispatch = useDispatch();
-    const { user, isLoggedIn } = useSelector((state) => state.auth);
-
-    const handleLogout = () => {
-        dispatch(logout());
-    };
+export default function Header({ user, logOut }) {
     return (
         <div className="App-header">
             <img src={logo} className="App-logo" alt="holberton logo" />
             <h1>School Dashboard</h1>
-            {isLoggedIn ? (
+            {user.isLoggedIn ? (
                 <div id="logoutSection">
-                    Welcome <b>{user.email}</b> <a href="#" onClick={handleLogout}>logout</a>
+                    Welcome <b>{user.email}</b> <a href="#" onClick={logOut}>logout</a>
                 </div>
             ) : null}
         </div>
