d5b3285303244a6169169925c442186f2837f211
Miteto08 <julien.lechevalier@shortways.com>
2025-05-09 10:54:33 +0200

    task7 - Async actions & Thunk middleware

--- react_redux_connectors_and_providers/task_2/dashboard/src/index.js
+++ react_redux_connectors_and_providers/task_2/dashboard/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import uiReducer from './reducers/uiReducer';
 import App from './App/App';
+import thunk from 'redux-thunk';
 
-const store = createStore(uiReducer);
+const store = createStore(uiReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
